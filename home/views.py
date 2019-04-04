9aec364c86ea14782bf37bf7758f4d16c07c178a
hande <queiroz.randerson@gmail.com>
2019-04-04 14:25:03 -0400

    TemplateViews

--- home/views.py
+++ home/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from django.views.generic.base import TemplateView
+
 
 def home(request):
     return render(request, 'home.html')
 
+
 def my_logout(request):
     logout(request)
     return redirect('home')
+
+class HomePageView(TemplateView):
+    template_name = 'home2.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context ['minha_variavel'] = 'Ola, bem vindo ao curso avancado de Django'
+        return context
